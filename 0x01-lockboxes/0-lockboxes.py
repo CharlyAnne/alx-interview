8c22ea9bb7d62f532c50c522682eaf5d55be2e01
CharlyAnne <icharlieanne@gmail.com>
2023-07-06 01:52:19 +0100

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,7 +5,8 @@
 def look_next_opened_box(opened_boxes):
     """Looks for the next opened box
     Args:
-        opened_boxes (dict): Dictionary which contains boxes already opened
+        opened_boxes (dict): Dictionary which
+        contains boxes already opened
     Returns:
         list: List with the keys contained in the opened box
     """
