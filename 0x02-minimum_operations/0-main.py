63cc77dd831c1c4e88550b0796d0b7fdaa6585c4
CharlyAnne <icharlieanne@gmail.com>
2023-07-13 12:05:41 +0100

    pep8 fix

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -3,6 +3,15 @@
 Main file for testing
 """
 
+"""
+initialize ops to 0 to keep track of the number of op needed
+Check if i is a factor of n:
+    If yes, update the value of n by dividing it by i and increment the number of ops by i.
+    Repeat until i is no longer a factor of n.
+After the loop, if n is greater than 1, it means n itself is a prime number.
+In this case, n is added to the number of ops.
+"""
+
 minOperations = __import__('0-minoperations').minOperations
 
 n = 4
--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
 """Minimum operations"""
 
-"""
-initialize ops to 0 to keep track of the number of op needed
-Check if i is a factor of n:
-    If yes, update the value of n by dividing it by i and increment the number of ops by i.
-    Repeat until i is no longer a factor of n.
-After the loop, if n is greater than 1, it means n itself is a prime number.
-In this case, n is added to the number of ops.
-"""
-
-
-
-
 import math
+
+
 def minOperations(n):
     """Returns the fewest number of ops to complete a taskt"""
     ops = 0
