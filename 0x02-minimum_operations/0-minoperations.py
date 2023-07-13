9678dca07e1e72c85b593ab7a401eb6b3f723144
CharlyAnne <icharlieanne@gmail.com>
2023-07-13 11:26:15 +0100

    minops

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,25 +1,33 @@
-#!/usr/bin/python3
 """Minimum operations"""
 import math
 
 
 def minOperations(n):
+    """Returns the fewest number of ops to complete a taskt"""
+    """initialize ops to 0 to keep track of the number of op needed"""
+    ops = 0
     """
-    Returns the fewest number of ops to result in exactly n, H characters
+    Check if i is a factor of n:
+        If yes, update the value of n by dividing it by i and increment the number of ops by i.
+        Repeat until i is no longer a factor of n.
     """
-    op = 0
     for i in range(2, int(math.sqrt(n)) + 1):
         while n % i == 0:
             n //= i
-            op += i
+            ops += i
+    """
+    After the loop, if n is greater than 1, it means n itself is a prime number.
+    In this case, n is added to the number of ops.
+    """
     if n > i:
-        op += n
+        ops += n
 
-    return op
+    return ops
 
 
 n = 4
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
+print(minOperations(14))
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
