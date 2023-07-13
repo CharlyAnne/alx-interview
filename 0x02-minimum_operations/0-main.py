8fba974d13ff3089004cd60f42aa47481ede1240
CharlyAnne <icharlieanne@gmail.com>
2023-07-13 12:03:28 +0100

    fixed comment indents

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -8,5 +8,13 @@ minOperations = __import__('0-minoperations').minOperations
 n = 4
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
-n = 12
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+n = 2
+print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 74
+"""This will give 39: 2 + 37"""
+print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 24
+"""This will give 9: 2 + 2 + 2 + 3"""
+print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 """Minimum operations"""
+
+"""
+initialize ops to 0 to keep track of the number of op needed
+Check if i is a factor of n:
+    If yes, update the value of n by dividing it by i and increment the number of ops by i.
+    Repeat until i is no longer a factor of n.
+After the loop, if n is greater than 1, it means n itself is a prime number.
+In this case, n is added to the number of ops.
+"""
+
+
+
+
 import math
-
-
 def minOperations(n):
     """Returns the fewest number of ops to complete a taskt"""
-    """initialize ops to 0 to keep track of the number of op needed"""
     ops = 0
-    """
-    Check if i is a factor of n:
-        If yes, update the value of n by dividing it by i and increment the number of ops by i.
-        Repeat until i is no longer a factor of n.
-    """
+
     for i in range(2, int(math.sqrt(n)) + 1):
         while n % i == 0:
             n //= i
             ops += i
-    """
-    After the loop, if n is greater than 1, it means n itself is a prime number.
-    In this case, n is added to the number of ops.
-    """
-    if n > i:
+
+    if n > 1:
         ops += n
 
     return ops
