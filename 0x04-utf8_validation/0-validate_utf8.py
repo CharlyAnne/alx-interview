90b28723eb0215e5f2d8b4d3cc91714a3a62e4ad
CharlyAnne <icharlieanne@gmail.com>
2023-07-27 01:28:22 +0100

    fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,23 +4,11 @@
 
 def validUTF8(data):
     """function determines if given data is valid UTF-8 encoding"""
-    num_bytes = 0
+    if any(val < 0 or val >= 256 for val in data):
+        return False
 
-    for num in data:
-        if num_bytes == 0:
-            if num >> 7 == 0b0:  # 1-byte character (0xxxxxxx)
-                continue
-            elif num >> 5 == 0b110:  # 2-byte character (110xxxxx)
-                num_bytes = 1
-            elif num >> 4 == 0b1110:  # 3-byte character (1110xxxx)
-                num_bytes = 2
-            elif num >> 3 == 0b11110:  # 4-byte character (11110xxx)
-                num_bytes = 3
-            else:
-                return False
-        else:
-            if num >> 6 != 0b10:  # Check if the next byte is 10xxxxxx
-                return False
-            num_bytes -= 1
-
-    return num_bytes == 0
+    try:
+        decodedText = bytes(data).decode('utf-8')
+        return True
+    except UnicodeDecodeError:
+        return False
